3c085e13f09c2ca1de6b6f97c49a0e15e36d594d
purva <purvajg@gmail.com>
2021-01-31 20:12:51 -0800

    [Portal - 3] PHP Portal Config assignment

--- contactme.php
+++ contactme.php
@@ -1,6 +1,36 @@
 <?php include 'includes/header.php';?>       
 <!-- START LEFT COL -->
 <section>
- <h2 class="pageID">Contact</h2>
- 
+<body>
+    <main role="main">
+	 <header><h3>Contact Us!</h3></header>
+    <!-- <p>Inside the source of this file, you can un-comment and test 
+        2 different types of forms:
+        <div class="indent">
+            <ul>
+                <li><b>loadContact('simple.php');</b> - a simple contact form with reCAPTCHA</li>
+                <li><b>loadContact('multiple.php');</b> - a complex form with reCAPTCHA</li>
+            </ul>
+        </div>    
+    </p>     -->
+	<?php
+        /*
+         * Below are 2 different forms to be re-used       
+         * 
+         * Only use one at a time, comment out the other!       
+         *
+         */
+
+        include 'includes/contact_include.php'; #site keys & code here
+    
+        $toAddress = "purvajg@gmail.com";  //place your/your client's email address here
+        $toName = "Purva Dalvi"; //place your client's name here
+        $website = "http://purdal1.dreamhosters.com/";  //place NAME of your client's website
+
+        echo loadContact('simple.php');#demonstrates a simple contact form
+        //echo loadContact('multiple.php');#demonstrates multiple form elements
+
+	?>
+     </main>
+</body> 
 <?php include 'includes/footer.php';?>
--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,8 @@
+<?php include 'portal-config.php'?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>IT_162</title>
+    <title><?=$title?></title>
     <meta name="viewport" content="width=device-width" />
     <meta name="robots" content="noindex,nofollow" />
     <meta charset="utf-8">
@@ -15,15 +16,10 @@
 <!-- START WRAPPER -->
 <main class="wrapper">
 <header>
-  <h1><a href="../index.php"><i class="logo fa fa-home"></i>Purva Dalvi</a></h1>
+  <h1><a href="../index.php"><i class="logo fa <?=$logo?>"<?=$logo_color?>></i>Purva Dalvi</a></h1>
   <nav>
     <ul class="topnav" id="myTopnav">
-      <li><a href="../index.php" class="selected">Welcome</a></li>
-      <li><a href="../big/index.php">Big</a></li>
-      <li><a href="../aia.php">AIA</a></li>
-      <li><a href="../flowchart.php">Flowchart/Layout</a></li>
-      <li><a href="../fp/index.php">Final Project</a></li>
-      <li><a href="../contactme.php">Contact Purva</a></li>
+    <?=makeLinks($nav1)?>
       <li class="icon"> <a href="javascript:void(0);" onclick="myFunction()">&#9776;</a> </li>
     </ul>
   </nav>
