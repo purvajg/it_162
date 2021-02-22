7dc974e095c34115214862be4987e21a6beeeb2b
purva <purvajg@gmail.com>
2021-02-21 20:56:03 -0800

    WiP for flexbox

---
+++ big/flexbox.php
@@ -0,0 +1,142 @@
+<?php include 'includes/header.php';?>  
+
+<section>
+<h2 class="pageID">Flexbox : </h3>
+ <p>
+ <ul>Properties of Parent :
+    <li>display</li>
+    <li>flex-direction</li>
+    <li>flex-wrap</li>
+    <li>flex-flow</li>
+    <li>justify-content</li>
+    <li>align-itemsp</li>
+    <li>align-content</li>
+    </ul>
+</p>
+
+<p>
+<ul>Properties of Children :
+    <li>order</li>
+    <li>flex-grow</li>
+    <li>flex-shrink</li>
+    <li>flex-basis</li>
+    <li>flex</li>
+    <li>align-self</li>
+</ul>
+</p>
+
+<p>
+<ul>The two axis :
+    <li>Main axis</li>
+    <li>Cross axis</li>
+</ul>
+</p> 
+
+
+<h2>Browser support chart for flexbox:</h2>
+<div>
+ <img src="images/browser_support.png" class="tablet" alt="" />
+</div>
+
+ <p>This gallery looks good to me for the project, it has a grid-based layout :</p>
+    <a href = "https://codepen.io/kathykato/pen/KRQOKY">https://codepen.io/kathykato/pen/KRQOKY</a>
+ <p>With an image gallery, images and slideshows can be combined together with easy navigation and cool transition effects. 
+   
+
+ <p>This w3 page gives tutorial of a grid based gallery just like above:</p>
+ <a href ="https://www.w3schools.com/css/css_image_gallery.asp">https://www.w3schools.com/css/css_image_gallery.asp</a>
+</p>
+
+<p></p>
+<p></p>
+<p></p>
+<h3>What are jQuery sliders:</h3>
+<p>
+    <ul>
+    jQuery sliders offer a simple, elegant way to display content without
+    ing up valuable real estate on the web page.
+<li>An image on the web just had one way of showing up: as a flat JPG, GIF or 
+PNG before using jQuery sliders. By using jQuery display multiple content 
+items can be displayed in a shared space on the page.</li>
+<li>The slider goes by a wide array of aliases: gallery, carousel, promos and 
+rotators but they all provide the same general feature.
+jQuery sliders can be used for nearly anything but most are used to 
+display a number of photos in a slideshow format.</li>
+<li>Many offer a number of 
+transition effects between slides and can be controlled by previous/next 
+buttons, tabbed or thumbnail navigation</li>
+</ul>
+</p>
+
+<ul>
+    <p>Top 3 Styles of jQuery Sliders according to our usage:</p>
+    <li>Standard Photo Slider</li>
+    <li>Mixed Content Slider</li>
+    <li>Gallery Style Slider</li>
+</ul>
+
+
+<h3>Choosing the slider:</h3>
+<p>Choosing a slider is largely dependent on the functionality and options you need and the overall style you are going for. Decide on the type of content you need displayed, how you want users to navigate through it and see which slider best matches your requirements. 
+Once you have narrowed down your choices, compare the overall script 
+size and check whether it is compatible with all browsers.
+Some sliders are offered in a "lite" version such as jQuery Cycle Lite if you're looking to keep the overall file size down in your project. Oftentimes, the light version is just missing the additional transition effects, sticking with the most popular options available.
+ </p>
+
+ <h3>Mobile sliders:</h3>
+ <p>For using sliders on phone, plugins are needed to be used.</p>
+
+
+ <h3>Research:</h3>
+ <ul>
+     <li>best html galleries: <a href="https://www.google.com/search?q=best+html+galleries&rlz=1C5CHFA_enUS872US872&oq=best+html+galleries&aqs=chrome.0.69i59j0i433j0j0i433j0j69i60l3.1689j0j9&sourceid=chrome&ie=UTF-8
+">https://www.google.com/search?q=best+html+galleries&rlz=1C5CHFA_enUS872US872&oq=best+html+galleries&aqs=chrome.0.69i59j0i433j0j0i433j0j69i60l3.1689j0j9&sourceid=chrome&ie=UTF-8
+</a></li>
+     <li>html galleries
+<a href="https://www.google.com/search?q=html+galleries&rlz=1C5CHFA_enUS872US872&oq=html+galleries&aqs=chrome.0.69i59j0i22i30l3.2725j0j7&sourceid=chrome&ie=UTF-8
+">https://www.google.com/search?q=html+galleries&rlz=1C5CHFA_enUS872US872&oq=html+galleries&aqs=chrome.0.69i59j0i22i30l3.2725j0j7&sourceid=chrome&ie=UTF-8
+</a></li>
+     <li>what is jquery sliders
+     <a href="https://www.google.com/search?safe=active&rlz=1C5CHFA_enUS872US872&sxsrf=ALeKk01YaZ0DsKe9GBMxC3LmCqkRTzSIyw%3A1612917117747&ei=fSkjYKmdLc-X-gS5_KGgAQ&q=what+is+jquery+sliders&oq=what+is+jquery+sliders&gs_lcp=CgZwc3ktYWIQAzoFCAAQzQJQuTBYhjdgxkVoAHACeACAAWeIAbwEkgEDNy4xmAEAoAEBqgEHZ3dzLXdpesABAQ&sclient=psy-ab&ved=0ahUKEwjpz-u-iN7uAhXPi54KHTl-CBQQ4dUDCA0&uact=5">https://www.google.com/search?safe=active&rlz=1C5CHFA_enUS872US872&sxsrf=ALeKk01YaZ0DsKe9GBMxC3LmCqkRTzSIyw%3A1612917117747&ei=fSkjYKmdLc-X-gS5_KGgAQ&q=what+is+jquery+sliders&oq=what+is+jquery+sliders&gs_lcp=CgZwc3ktYWIQAzoFCAAQzQJQuTBYhjdgxkVoAHACeACAAWeIAbwEkgEDNy4xmAEAoAEBqgEHZ3dzLXdpesABAQ&sclient=psy-ab&ved=0ahUKEwjpz-u-iN7uAhXPi54KHTl-CBQQ4dUDCA0&uact=5</a>       
+</li>
+     <li>what is mobile jquery sliders
+    <a href="https://www.google.com/search?safe=active&rlz=1C5CHFA_enUS872US872&sxsrf=ALeKk00qPEFXPtKsXjQOW0QTk0AO0Q6whg%3A1612917230703&ei=7ikjYPzMKsXt-gTHnLqYDw&q=what+is+mobile+jquery+sliders&oq=what+is+mobile+jquery+sliders&gs_lcp=CgZwc3ktYWIQAzoHCCMQsAMQJzoHCAAQRxCwAzoHCCMQsAIQJzoECCEQClD0kARYhp4EYP6iBGgDcAJ4AIABswGIAbsFkgEDNy4xmAEAoAEBqgEHZ3dzLXdpesgBCcABAQ&sclient=psy-ab&ved=0ahUKEwj8-9n0iN7uAhXFtp4KHUeODvMQ4dUDCA0&uact=5
+">https://www.google.com/search?safe=active&rlz=1C5CHFA_enUS872US872&sxsrf=ALeKk00qPEFXPtKsXjQOW0QTk0AO0Q6whg%3A1612917230703&ei=7ikjYPzMKsXt-gTHnLqYDw&q=what+is+mobile+jquery+sliders&oq=what+is+mobile+jquery+sliders&gs_lcp=CgZwc3ktYWIQAzoHCCMQsAMQJzoHCAAQRxCwAzoHCCMQsAIQJzoECCEQClD0kARYhp4EYP6iBGgDcAJ4AIABswGIAbsFkgEDNy4xmAEAoAEBqgEHZ3dzLXdpesgBCcABAQ&sclient=psy-ab&ved=0ahUKEwj8-9n0iN7uAhXFtp4KHUeODvMQ4dUDCA0&uact=5
+</a>
+</li>
+
+</ul>
+
+
+<h3>Gather:</h3>
+ <ul>
+     <li>Hexagonal, grid gallery, flip elements gallery, pop in gallery, before and after effect gallery.
+All the above with making the gallery responsive.
+<a href="https://wpdatatables.com/css-image-gallery/
+">https://wpdatatables.com/css-image-gallery/
+</a>
+</li>
+     <li>Standard Photo Slider(for beginners), Mixed Content Slider(video, HTML and image content within the same slider), Gallery Style Slider(easy browsing). 
+Briefs about deciding on which kind of slider to use. Also gives details about integrating slider with existing website
+<a href ="https://webdesign.tutsplus.com/articles/how-to-start-using-jquery-sliders-in-your-designs--webdesign-380#:~:text=jQuery%20sliders%20offer%20a%20simple,one%20into%20a%20demo%20site.">https://webdesign.tutsplus.com/articles/how-to-start-using-jquery-sliders-in-your-designs--webdesign-380#:~:text=jQuery%20sliders%20offer%20a%20simple,one%20into%20a%20demo%20site.</a>
+</li>
+     <li>Gives a list of plugins for sliders on mobile
+         <a href ="https://365webresources.com/15-best-responsive-mobile-friendly-jquery-slider-plugins/">https://365webresources.com/15-best-responsive-mobile-friendly-jquery-slider-plugins/</a>
+     </li>
+     <li></li>
+
+</ul>
+</section>
+
+<!-- DIVIDER -->
+
+<aside>
+<h3>Citations:</h3>
+<ul>
+<li>Coyier, Chris. "A Complete Guide to Flexbox" for CSS tricks | Content on CSS. Dec 21, 2010.
+</li>
+
+<li>Maldonado, Leonardo. "When to use Flexbox and when to use CSS grid" for Log Rocket | Content on CSS. April 6, 2020.</li>
+</ul>
+</aside>
+<?php include 'includes/footer.php';?>
---
+++ big/includes/header.php
@@ -0,0 +1,43 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Big Project</title>
+    <meta name="viewport" content="width=device-width" />
+    <meta name="robots" content="noindex,nofollow" />
+    <meta charset="utf-8">
+    <script src="https://use.fontawesome.com/6a71565c22.js"></script>
+    <link rel="stylesheet" href="css/nav.css" />
+    <link rel="stylesheet" href="css/big.css" />
+    <link rel="stylesheet" href="css/form.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+    <script src="https://s3.amazonaws.com/menumaker/menumaker.min.js" type="text/javascript"></script>
+    <script src="js/script.js"></script>
+</head>
+
+<body>
+<!-- START WRAPPER -->
+<main class="wrapper"> <!-- she has put div in place on main here -->
+<header>
+  <h1><a href="../index.php"><i class="logo fa fa-home"></i>Big Project</a></h1>
+  <nav id="cssmenu">
+  <ul>
+     <li><a href="../index.php"><span><i class="fa fa-fw fa-bank"></i> WEB162   </span></a></li>
+     <li><a href="index.php"><span><i class="fa fa-fw fa-home"></i> BIG</span></a></li>
+     <li><a href="flexbox.php"><span><i class="fa fa-fw fa-cube"></i> Flexbox</span></a></li>
+     <li><a href="galleries.php"><span><i class="fa fa-fw fa-camera-retro"></i> Galleries</span></a></li>
+     <li><a href="#"><span><i class="fa fa-fw fa-chevron-down"></i> Google</span></a>
+        <ul>
+           <li><a href="calendar.php"><span><i class="fa fa-fw fa-calendar"></i> Calendar</span></a></li>
+           <li><a href="map.php"><span><i class="fa fa-fw fa-map-o"></i> Map</span></a></li>
+           <li><a href="youtube.php"><span><i class="fa fa-fw fa-youtube-square"></i> YouTube</span></a></li>
+        </ul>
+     </li>
+     <li><a href="shoppingcarts.php"><span><i class="fa fa-fw fa-shopping-basket"></i> Shopping Carts</span></a></li>
+     <li><a href="siteapp.php"><span><i class="fa fa-fw fa-html5"></i> Site vs App</span></a></li>
+     <li><a href="webcam.php"><span><i class="fa fa-fw fa-eye"></i> Web Cam</span></a></li>
+  </ul>
+  </nav>
+</header>
+<!-- HEADER ENDS HERE -->   
