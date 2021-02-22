9e3be7caf303e0d4098680dc69e96181c6063c5c
purva <purvajg@gmail.com>
2021-02-21 20:29:54 -0800

    WIP midterm

--- big/index.php
+++ big/index.php
@@ -1,6 +1,24 @@
-<?php include '../includes/header.php';?>       
+
+<?php include 'includes/header.php';?>  
 <!-- START LEFT COL -->
 <section>
- <h2 class="pageID">Big</h2>
- 
-<?php include '../includes/footer.php';?>
+ <h2 class="pageID">Client Questionnaire</h3>
+ <img src="images/tablet.jpg" class="tablet" alt="" />
+ <p>Develop and place an updated version of the "multiple.php" contact form (located in your web120 includes folder) on this main page. Adjust its fields to ask the basic questions you'd always need to ask any client to help determine if you can help them with their website needs.</p>
+ <p></p>
+</section>
+<!-- END LEFT COL -->
+
+
+<!-- START RIGHT COL -->
+<aside>
+ <h3>Basic Website Design Cycle</h3>
+ <img src="images/tablet.jpg" class="tablet" alt="" />
+ <p>Provide at least 3 (linked) resources that help you and/or your web team with steps found in very basic website design and development cycle. For example, client surveys, wireframing tools, etc.</p>
+ <p>Here's a website to help get you started,</p>
+ <p>
+ <a href ="https://flourishdesignstudio.com/get-started/new-client-questionnaire/" target ="_top">https://flourishdesignstudio.com/get-started/new-client-questionnaire/</a></p>
+</aside>
+<!-- END RIGHT COL -->
+
+<?php include 'includes/footer.php';?>
