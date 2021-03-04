f130bc2da51c1e53730dd1188aecacb7ab8b10da
purva <purvajg@gmail.com>
2021-03-03 23:44:58 -0800

    Final changes

--- aia.php
+++ aia.php
@@ -1,6 +1,109 @@
 <?php include 'includes/header.php';?>       
-<!-- START LEFT COL -->
-<section>
- <h2 class="pageID">AIA</h2>
+
+<section style="width:100%">
+ <h2 class="pageID">Audience, Issues and Approach (AIA)</h2>
+ 
+ <div class="container">
+  <!-- <h2>Client Information </h2> -->
+  <ul class="responsive-table">
+    <li class="table-header">
+      <div class="col col-2">Client Information</div>
+    </li>
+    <li class="table-row">
+      <div class="col col-1" data-label="Job Id">Client Name & Decision Maker</div>
+      <div class="col col-2" data-label="Customer Name">R.Danny</div>
+    </li>
+    <li class="table-row">
+      <div class="col col-1" data-label="Job Id">Business Name</div>
+      <div class="col col-2" data-label="Customer Name">TheGupShopApp.com</div>
+    </li>
+    <li class="table-row">
+      <div class="col col-1" data-label="Job Id">Redesign or New Site</div>
+      <div class="col col-2" data-label="Customer Name">New site</div>
+    </li>
+    <li class="table-row">
+      <div class="col col-1" data-label="Job Id">Type of Website</div>
+      <div class="col col-2" data-label="Customer Name">Landing page for a mobile app</div>
+    </li>
+    <li class="table-row">
+      <div class="col col-1" data-label="Job Id">Client's Goals</div>
+      <div class="col col-2" data-label="Customer Name">A solid landing page that'll explain what the app does and why the customer needs it in the most engaging and convincing way possible.</div>
+    </li>
+  </ul>
+</div>
+
+
+
+<div class="container-big">
+  <!-- <h2>Client Information </h2> -->
+  <ul class="responsive-table-big">
+    <li class="table-header">
+      <div class="col col-1" style="color:black">Audiences</div>
+      <div class="col col-2">Issues, Concerns, Needs</div>
+      <div class="col col-4">Approach</div>
+    </li>
+    <li class="table-row">
+      <div class="col col-1" data-label="Job Id">Service providers
+      <small>TheGupShopApp.com 
+          provides services provided by local professionals to customers 
+          by matching them. Many types of services in categories such as 
+          home, wellness, events, and lessons.
+      </small>
+      </div>
+      <div class="col col-2" data-label="Customer Name">
+         
+              Explaing the variety of services offered by the service providers in the app
+          
+      
+           </div>
+      <div class="col col-3"> 
+              As this site is the landing page for the app, the website would show
+          a screenshot of the page which showcases the screen where services
+          are shown
+            A textbox or a container showing a description of services
+                offered and the reviews of some of the consumers would be shown
+            
+    
+      </div>
+    </li>
+
+    <li class="table-row">
+      <div class="col col-1" data-label="Job Id">Service consumers
+      <small>Consumers would search for service providers based on the location and categories
+      </small>
+      </div>
+      <div class="col col-2" data-label="Customer Name">
+          
+              Explaing how the consumers can make use of the services provided
+          
+      
+           </div>
+      <div class="col col-3"> 
+         
+              Giving screenshot of the page that shows a list of service providers which has their name, total number of reviews and rating
+        
+      </div>
+    </li>
+
+   
+    <li class="table-row">
+      <div class="col col-1" data-label="Job Id">Messenger feature users
+      <small>The users of the app would be using the feature of chat to chat with the service providers or with their friends
+      </small>
+      </div>
+      <div class="col col-2" data-label="Customer Name">
+          
+              Explaing how the consumers can make use of the messenger service
+         
+      
+           </div>
+      <div class="col col-3"> 
+          
+             Giving screenshot of the page that shows the messenger screens like chat list or a chat with someone
+        
+      </div>
+    </li>
+  </ul>
+</div>
  
 <?php include 'includes/footer.php';?>
