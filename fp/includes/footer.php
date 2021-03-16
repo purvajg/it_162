538db21537508eb1a420c3dbff53159e55274cd3
purva <purvajg@gmail.com>
2021-03-16 15:31:49 -0700

    Added index.php, css folder, js folder images folder and includes folder in fp folder.Added header.php, footer.php and fp-config.php in includes folder

---
+++ fp/includes/footer.php
@@ -0,0 +1,92 @@
+<!-- footer section starts  -->
+
+<section class="footer">
+
+    <div class="box-container" id="contactUs">
+
+        <div class="box">
+            <h3>why choose us?</h3>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur, facere iusto excepturi at modi illo tenetur atque fugiat numquam rerum.</p>
+        </div>
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="#home">home</a>
+            <a href="#feature">feature</a>
+            <a href="#screenshot">screenshot</a>
+            <a href="#review">review</a>
+            <a href="#plan">plan</a>
+            <a href="#download">download</a>
+            <a href="#contactUs">contact us</a>
+        </div>
+
+        
+        <div class="box">
+            <h3>Contact us</h3>
+            <p>Get in touch with us</p>
+            <form action="#" class="form">
+                <div class="form">
+                    <label for="name"></label>
+                    <input type="text" name="name" id="name" required placeholder="enter your name">       
+                </div>
+
+                <div class="form">
+                    <label for="email"></label>
+                    <input type="email" name="email" id="email" required placeholder="enter your email">
+                </div>
+
+                <div class="form">
+                        <label for="comments"></label>
+                        <input type="text" name="comments" id="comments" required placeholder="Comments.." style="height:200px">
+                    </div>
+
+                <div class="form">
+                    <button class="fas fa-paper-plane"></button>
+                </div>
+            </form>
+        </div>
+
+    </div>
+
+    <h1 class="credit">created by <a href="#">Purva Dalvi</a>| All rights reserved. </h1>
+    <h1 class="credit"><a href="https://validator.w3.org/nu/?doc=http%3A%2F%2Fpurdal1.dreamhosters.com%2Ftemplate.php">Valid HTML</a></h1>
+    <h1 class="credit"><a href="https://jigsaw.w3.org/css-validator/validator?uri=http%3A%2F%2Fpurdal1.dreamhosters.com%2Ftemplate.php&profile=css3svg&usermedium=all&warning=1&vextwarning=&lang=en">Valid CSS</a></h1>
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- jquery cdn link  -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- owl carousel js cdn link  -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/main.js"></script>
+
+</body>
+</html>
--- fp/index.php
+++ fp/index.php
@@ -1,6 +1,286 @@
-<?php include '../includes/header.php';?>       
-<!-- START LEFT COL -->
-<section>
- <h2 class="pageID">Final</h2>
- 
-<?php include '../includes/footer.php';?>
+<?php include 'includes/header.php';?>    
+
+
+
+<!-- share icons  -->
+<div id="share">
+    <i class="fas fa-share"></i>
+    <a href="#" class="fab fa-facebook-f"></a>
+    <a href="#" class="fab fa-twitter"></a>
+    <a href="#" class="fab fa-instagram"></a>
+    <a href="#" class="fab fa-linkedin"></a>
+    <a href="#" class="fab fa-pinterest"></a>
+</div>
+
+
+
+<!-- home section starts  -->
+<section class="home" id="home">
+
+<div class="content">
+    <h1>the app</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non consectetur suscipit aut ut eos odio nobis minima officiis mollitia tempore.</p>
+    <div class="buttons">
+        <button class="btn" onclick="#">download</button>
+        <button class="btn" onclick="#">learn more</button>
+    </div>
+</div>
+
+<div class="image">
+    <img src="images/main-img.png" alt="">
+</div>
+
+</section>
+<!-- home section ends -->
+
+
+
+<!-- feature section starts  -->
+<section class="feature" id="feature">
+
+<h1 class="heading"><span>app features</span></h1>
+
+<div class="box-container">
+
+    <div class="box">
+        <i class="fas fa-sync-alt"></i>
+        <h3>free updates</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, fuga alias ea quis repudiandae modi architecto reiciendis fugit libero sunt.</p>
+    </div>
+
+    <div class="box">
+        <i class="fas fa-headset"></i>
+        <h3>app support</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, fuga alias ea quis repudiandae modi architecto reiciendis fugit libero sunt.</p>
+    </div>
+
+    <div class="box">
+        <i class="fas fa-cogs"></i>
+        <h3>easy settings</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, fuga alias ea quis repudiandae modi architecto reiciendis fugit libero sunt.</p>
+    </div>
+
+    <div class="box">
+        <i class="fas fa-shield-alt"></i>
+        <h3>privacy protect</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, fuga alias ea quis repudiandae modi architecto reiciendis fugit libero sunt.</p>
+    </div>
+
+    <div class="box">
+        <i class="far fa-bell"></i>
+        <h3>notifications</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, fuga alias ea quis repudiandae modi architecto reiciendis fugit libero sunt.</p>
+    </div>
+
+    <div class="box">
+        <i class="far fa-credit-card"></i>
+        <h3>in app purchase</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, fuga alias ea quis repudiandae modi architecto reiciendis fugit libero sunt.</p>
+    </div>
+
+</div>
+
+</section>
+<!-- feature section ends -->
+
+
+
+<!-- screenshot section starts  -->
+<section class="screenshot" id="screenshot">
+
+<h1 class="heading"><span>app screenshot</span></h1>
+
+<div class="screen-slider owl-carousel">
+
+    <div class="item">
+        <img src="images/img1.png" alt="">
+    </div>
+    <div class="item">
+        <img src="images/img2.png" alt="">
+    </div>
+    <div class="item">
+        <img src="images/img3.png" alt="">
+    </div>
+    <div class="item">
+        <img src="images/img4.png" alt="">
+    </div>
+    <div class="item">
+        <img src="images/img5.png" alt="">
+    </div>
+
+</div>
+
+</section>
+<!-- screenshot section ends -->
+
+
+
+<!-- review section starts  -->
+<section class="review" id="review">
+
+<h1 class="heading"><span>customers review</span></h1>
+
+<div class="review-slider owl-carousel">
+
+    <div class="box">
+        <img src="images/pic1.png" alt="">
+        <h3>john deo</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, voluptates mollitia! Omnis porro deleniti modi sed ducimus voluptate, tenetur nihil voluptatem repellendus pariatur dignissimos atque dolorum eligendi odit ex quisquam?</p>
+        <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+        </div>
+    </div>
+
+    <div class="box">
+        <img src="images/pic2.jpeg" alt="">
+        <h3>john deo</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, voluptates mollitia! Omnis porro deleniti modi sed ducimus voluptate, tenetur nihil voluptatem repellendus pariatur dignissimos atque dolorum eligendi odit ex quisquam?</p>
+        <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+        </div>
+    </div>
+
+    <div class="box">
+        <img src="images/pic3.webp" alt="">
+        <h3>john deo</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, voluptates mollitia! Omnis porro deleniti modi sed ducimus voluptate, tenetur nihil voluptatem repellendus pariatur dignissimos atque dolorum eligendi odit ex quisquam?</p>
+        <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+        </div>
+    </div>
+
+    <div class="box">
+        <img src="images/pic4.webp" alt="">
+        <h3>john deo</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, voluptates mollitia! Omnis porro deleniti modi sed ducimus voluptate, tenetur nihil voluptatem repellendus pariatur dignissimos atque dolorum eligendi odit ex quisquam?</p>
+        <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+        </div>
+    </div>
+
+</div>
+
+</section>
+<!-- review section ends -->
+
+
+
+<!-- plan section starts  -->
+<section class="plan" id="plan">
+
+<h1 class="heading"><span>choose a plan</span></h1>
+
+<div class="box-container">
+
+    <div class="box">
+        <h3 class="title">basic</h3>
+        <h3 class="price">$10<span>/mo</span></h3>
+        <ul class="list">
+            <li> <i class="fas fa-check"></i> full access </li>
+            <li> <i class="fas fa-check"></i> full support </li>
+            <li> <i class="fas fa-times"></i> new themes </li>
+            <li> <i class="fas fa-times"></i> admin panel </li>
+            <li> <i class="fas fa-times"></i> unlimited package </li>
+        </ul>
+        <button class="btn" onclick="#">check out</button>
+    </div>
+
+    <div class="box">
+        <h3 class="title">standard</h3>
+        <h3 class="price">$20<span>/mo</span></h3>
+        <ul class="list">
+            <li> <i class="fas fa-check"></i> full access </li>
+            <li> <i class="fas fa-check"></i> full support </li>
+            <li> <i class="fas fa-check"></i> new themes </li>
+            <li> <i class="fas fa-times"></i> admin panel </li>
+            <li> <i class="fas fa-times"></i> unlimited package </li>
+        </ul>
+        <button class="btn" onclick="#">check out</button>
+    </div>
+
+    <div class="box">
+        <h3 class="title">premium</h3>
+        <h3 class="price">$30<span>/mo</span></h3>
+        <ul class="list">
+            <li> <i class="fas fa-check"></i> full access </li>
+            <li> <i class="fas fa-check"></i> full support </li>
+            <li> <i class="fas fa-check"></i> new themes </li>
+            <li> <i class="fas fa-check"></i> admin panel </li>
+            <li> <i class="fas fa-check"></i> unlimited package </li>
+        </ul>
+        <button class="btn" onclick="#">check out</button>
+    </div>
+
+</div>
+
+</section>
+<!-- plan section ends -->
+
+
+
+<!-- download section starts  -->
+<section class="download" id="download">
+
+<h1 class="heading"><span>download the app</span></h1>
+
+<div class="box-container">
+
+    <div class="box">
+        <i class="fab fa-google-play"></i>
+        <h3>google play</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque ex delectus, sequi asperiores hic dolorum eius deleniti? Necessitatibus, aliquid officiis?</p>
+        <button class="btn" onclick="#">download</button>
+    </div>
+
+    <div class="box">
+        <i class="fab fa-windows"></i>
+        <h3>microsoft store</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque ex delectus, sequi asperiores hic dolorum eius deleniti? Necessitatibus, aliquid officiis?</p>
+        <button class="btn" onclick="#">download</button>
+    </div>
+
+    <div class="box">
+        <i class="fab fa-apple"></i>
+        <h3>apple store</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque ex delectus, sequi asperiores hic dolorum eius deleniti? Necessitatibus, aliquid officiis?</p>
+        <button class="btn" onclick="#">download</button>
+    </div>
+
+</div>
+
+</section>
+<!-- download section ends -->
+
+
+
+<!-- <section class="contactUs" id="contactUs"> -->
+   
+
+        <!-- // include 'includes/contact_include.php'; #site keys & code here
+    
+        // $toAddress = "purvajg@gmail.com";  //place your/your client's email address here
+        // $toName = "Purva Dalvi"; //place your client's name here
+        // $website = "http://purdal1.dreamhosters.com/";  //place NAME of your client's website
+
+        // echo loadContact('simple.php');#demonstrates a simple contact form -->
+
+
+<!-- </section> -->
+
+<!-- <?php include 'includes/footer.php';?> -->
